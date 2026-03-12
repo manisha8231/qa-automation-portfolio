7bdbda9bf2524a1047904ec653433a5a30f76bff
Manisha <manisha8231@gmail.com>
2026-03-12 13:07:01 -0600

    Remove old Playwright test files from root

--- fetch.js
+++
@@ -1,6 +0,0 @@
-const https = require('https');
-https.get('https://automationexercise.com/login', res=>{
-  let data='';
-  res.on('data',chunk=>data+=chunk);
-  res.on('end',()=> console.log(data.slice(0,5000)));
-});
--- screenshotLogin.js
+++
@@ -1,9 +0,0 @@
-const { chromium } = require('playwright');
-(async () => {
-  const browser = await chromium.launch();
-  const page = await browser.newPage();
-  await page.goto('https://automationexercise.com/login');
-  await page.screenshot({ path: 'login.png', fullPage: true });
-  console.log('screenshot saved');
-  await browser.close();
-})();
